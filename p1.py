234af95a5a5929e62b7beb8d0b8b837d2d55a42c
jerry34567 <92587187+jerry34567@users.noreply.github.com>
2021-10-16 10:36:19 +0800

    Update p1.py

--- p1.py
+++ p1.py
@@ -14,6 +14,14 @@ def decode(cipher, key):
     Return:
         plain: str, plain text
     '''
-
     plain = ''
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,?!'
+    public_key = np.reshape(key, (-1, 3)).T
+    private_key = mod_inv(public_key)
+    cipher_text = np.reshape(cipher, (-1, 3)).T
+    temp = np.mod(private_key @ cipher_text, 31)
+    for i in range (0, temp.shape[1]):
+        for j in range (0, temp.shape[0]):
+            plain.append(letters[temp[j][i]])
+    
     return plain
